2e939f084cc4de29d8c90fd5cd99969ae4edf636
Anzar <peerzada.anzar@gmail.com>
2022-01-02 20:30:16 +0530

    Team Page

---
+++ team/intro.php
@@ -0,0 +1,17 @@
+
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+  
+
+  
+</head>
+<body>
+   <center> <h1>The Team</h1></center>
+<a href="http://localhost/quiz/final/" ><img style="height: 100%;
+    width: 100%;" src="a.jpg">
+</a>
+</body>
+</html>
+
